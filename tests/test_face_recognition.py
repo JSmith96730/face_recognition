e37edcd114daf595705a4cc6483f7cffe538ab11
John Smith <ksgkdu96@gmail.com>
2026-02-23 02:38:09 +0800

    modify face_encoding dimention

--- tests/test_face_recognition.py
+++ tests/test_face_recognition.py
@@ -157,7 +157,7 @@ class Test_face_recognition(unittest.TestCase):
         encodings = api.face_encodings(img, model='large')
 
         self.assertEqual(len(encodings), 1)
-        self.assertEqual(len(encodings[0]), 128)
+        self.assertEqual(len(encodings[0]), 256)
 
     def test_face_distance(self):
         img_a1 = api.load_image_file(os.path.join(os.path.dirname(__file__), 'test_images', 'obama.jpg'))
